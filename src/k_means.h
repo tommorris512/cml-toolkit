68c0e93d51c742a71713c9a757f2033991154d96
Tom Morris <tommorris512@icloud.com>
2024-07-07 01:04:52 +0100

    Created header for k-means model

---
+++ src/k_means.h
@@ -0,0 +1,38 @@
+#ifndef K_MEANS_H
+#define K_MEANS_H
+
+/*
+ * Define a typed struct to encapsulate KMeans models.
+ */
+typedef struct {
+    double** centroids;
+    int k;
+    int num_features;
+} KMeans;
+
+/* FUNCTION PROTOTYPES */
+
+/*
+ * Creates a new KMeans for a specified number of clusters and features.
+ * Returns a pointer to a new KMeans on success and NULL on failure.
+ */
+KMeans* create_k_means(int k, int num_features);
+
+/*
+ * Fits the KMeans model to a series of data samples.
+ * This redistributes the centroids of the model based on the data samples given.
+ */
+void fit_k_means(KMeans* km, double** X, int num_samples, int num_iterations);
+
+/*
+ * Predicts the cluster of a given data point.
+ * Returns the predicted cluster number based on the model's centroids.
+ */
+int predict_k_means(KMeans* km, double* X);
+
+/*
+ * Frees the dynamically allocated memory used by the KMeans model.
+ */
+void free_k_means(KMeans* km);
+
+#endif /* For K_MEANS_H */
